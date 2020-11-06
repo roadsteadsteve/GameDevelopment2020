fce1def4bf84dcd27ca1d25157cd6d1e9d7f8c33
PeriDom <dom.sschmieder@gmail.com>
2020-11-06 09:34:36 -0500

    More basic unity work and learning

---
+++ Unity/Unity Projects/MouseGirlMovement/Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	[SerializeField]
+	private float _speed;
+
+	[SerializeField]
+	private float _horizontalInput;
+
+	[SerializeField]
+	private float _verticalInput;
+
+	[SerializeField]
+	private float _jumpInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	_horizontalInput = Input.GetAxis("Horizontal");
+    	transform.Translate(new Vector3(_horizontalInput, 0, 0) * _speed * Time.deltaTime);
+
+    	_verticalInput = Input.GetAxis("Vertical");
+    	transform.Translate(new Vector3(0, 0, _verticalInput) * _speed * Time.deltaTime);
+
+    	_jumpInput = Input.GetAxis("Jump");
+    	transform.Translate(new Vector3(0, _jumpInput, 0) * _speed * Time.deltaTime);
+        
+    }
+}
---
+++ Unity/Unity Projects/MouseGirlMovement/Assets/Scripts/SampleAgent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleAgent : MonoBehaviour
+{
+
+	public Transform target;
+	UnityEngine.AI.NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       agent.SetDestination(target.position);
+    }
+}
