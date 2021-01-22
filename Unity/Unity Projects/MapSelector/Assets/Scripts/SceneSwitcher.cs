0692c1c8575798361309d2702342cc64b1352064
PeriDom <dom.sschmieder@gmail.com>
2021-01-22 11:04:19 -0500

    updates
    changing scenes through movement and buttons, animating mus

---
+++ Unity/Unity Projects/MapSelector/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("Great Forest Map");
+    }
+
+    public void GotoWastelandScene()
+    {
+        SceneManager.LoadScene("Wasteland Den");
+    }
+
+    public void GotoHollowCaveScene()
+    {
+        SceneManager.LoadScene("Hollow Cave");
+    }
+
+    public void GotoFlowerPatchScene()
+    {
+        SceneManager.LoadScene("Flower Patch");
+    }
+
+    public void GotoLittleBurrowScene()
+    {
+        SceneManager.LoadScene("Little Burrow");
+    }
+
+    public void GotoRaccoonVillageScene()
+    {
+        SceneManager.LoadScene("Raccoon Village");
+    }
+}
+
---
+++ Unity/Unity Projects/WalkingBetweenScenes/Assets/Scripts/LevelControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+
+	public int index;
+	public string levelName;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.CompareTag ("Player"))
+			{
+				//Loading Level with Build index
+				//SceneManager.LoadScene(index);
+
+				//Loading level with scene name
+				SceneManager.LoadScene(levelName);
+
+				//Restart 1v1
+				//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+			}
+	}
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
