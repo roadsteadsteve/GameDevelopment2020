e454c5f4dd4b5d57e5966bd0b1f9363de26a3f33
PeriDom <dom.sschmieder@gmail.com>
2021-01-29 10:59:01 -0500

    fixed onTrigger scene change with Mus

--- Unity/Unity Projects/WalkingBetweenScenes/Assets/Scripts/LevelControl.cs
+++ Unity/Unity Projects/WalkingBetweenScenes/Assets/Scripts/LevelControl.cs
@@ -10,24 +10,52 @@ public class LevelControl : MonoBehaviour
 	public int index;
 	public string levelName;
 
+	public GameObject MouseGirlRigged;
+
 	void OnTriggerEnter (Collider other)
 	{
-		if (other.CompareTag ("Player"))
-			{
-				//Loading Level with Build index
-				//SceneManager.LoadScene(index);
+		StartCoroutine(LoadYourAsyncScene());
 
-				//Loading level with scene name
-				SceneManager.LoadScene(levelName);
+		// Scene currentScene = SceneManager.GetActiveScene();
 
-				//Restart 1v1
-				//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-			}
+		// if (other.CompareTag ("Player"))
+		// 	{
+		// 		//Loading Level with Build index
+		// 		//SceneManager.LoadScene(index);
 
-	
-	
-		}
+		// 		//Loading level with scene name
+		// 		SceneManager.LoadScene(levelName);
 
+		// 		SceneManager.MoveGameObjectToScene(MouseGirlRigged, SceneManager.GetSceneByName("Raccoon Village"));
+  		// 		SceneManager.UnloadSceneAsync(currentScene);
+
+		// 		//Restart 1v1
+		// 		//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+			
+		// 	}
+
+	}
+
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Raccoon Village", LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(MouseGirlRigged, SceneManager.GetSceneByName("Raccoon Village"));
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+    }
 
 
     // Start is called before the first frame update
@@ -36,7 +64,6 @@ public class LevelControl : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
