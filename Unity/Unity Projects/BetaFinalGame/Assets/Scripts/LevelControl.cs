b2c0468d77e071aad0325916386f63d19d021147
PeriDom <dom.sschmieder@gmail.com>
2021-02-26 10:35:37 -0500

    Created new final game so the Unity version matches with the version Jack is using

---
+++ Unity/Unity Projects/BetaFinalGame/Assets/Scripts/LevelControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+
+	public int index;
+	public string levelName;
+
+	public GameObject MouseGirlRigged;
+
+	void OnTriggerEnter (Collider other)
+	{
+		StartCoroutine(LoadYourAsyncScene());
+
+		// Scene currentScene = SceneManager.GetActiveScene();
+
+		// if (other.CompareTag ("Player"))
+		// 	{
+		// 		//Loading Level with Build index
+		// 		//SceneManager.LoadScene(index);
+
+		// 		//Loading level with scene name
+		// 		SceneManager.LoadScene(levelName);
+
+		// 		SceneManager.MoveGameObjectToScene(MouseGirlRigged, SceneManager.GetSceneByName("Raccoon Village"));
+  		// 		SceneManager.UnloadSceneAsync(currentScene);
+
+		// 		//Restart 1v1
+		// 		//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+			
+		// 	}
+
+	}
+
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("SetUpScene", LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(MouseGirlRigged, SceneManager.GetSceneByName("SetUpScene"));
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
+
+
+
+
---
+++ Unity/Unity Projects/BetaFinalGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	private Rigidbody rb;
+	private float moveSpeed;
+	private float dirX, dirZ;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        moveSpeed = 3f;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirX = Input.GetAxis("Horizontal") * moveSpeed;
+        dirZ = Input.GetAxis("Vertical") * moveSpeed;
+
+        if (Input.GetKeyDown (KeyCode.Space)) {
+    		  transform.Translate(Vector3.up * Time.deltaTime*100);
+       	}
+    		
+   	    if (Input.GetKeyUp (KeyCode.Space)) {
+  			//transform.position=initialposition;
+      		transform.Translate(Vector3.down * Time.deltaTime*100);
+       		Debug.Log("Clicked");
+    	}
+   		
+    }
+
+    private void FixedUpdate() 
+    {
+    	rb.velocity = new Vector3(dirX, rb.velocity.y, dirZ);
+    }
+}
