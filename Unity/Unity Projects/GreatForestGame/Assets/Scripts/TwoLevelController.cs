ab053a680cf0b52c900159ba70694ce7b48c36a2
PeriDom <dom.sschmieder@gmail.com>
2021-03-05 10:36:30 -0500

    added boxes for colliders in game set up scene

---
+++ Unity/Unity Projects/GreatForestGame/Assets/Scripts/TwoLevelController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AnyManager : MonoBehaviour
+{
+ 	public static AnyManager anyManager;
+
+ 	bool gameStart;
+
+ 	void Awake ()
+ 	{
+ 		if (!gameStart)
+ 		{
+ 			anyManager = this;
+ 			SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+
+ 			gameStart = true;
+ 		}
+ 	}
+
+ 	public void UnloadScene(int scene)
+ 	{
+ 		StartCoroutine(Unload(scene));
+ 	}
+
+ 	IEnumerator Unload(int scene)
+ 	{
+ 		yield return null;
+
+ 		SceneManager.UnloadScene(scene);
+ 	}
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
